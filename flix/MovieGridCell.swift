3bf9a69df4b786a439f7079fb812b4b400db6a0c
dreamerlim <dreamerlim@gmail.com>
2021-09-27 20:13:07 -0700

    flix update detail grid

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Leo King on 9/27/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
